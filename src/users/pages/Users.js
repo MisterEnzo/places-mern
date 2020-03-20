1001827b9e9594f091c7a23d48304059b73bb94d
enz <enzo_naguiat@yahoo.com>
2020-03-20 18:47:15 +0900

    make card and avatar components. edit styling of user-item component

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -4,12 +4,12 @@ import UsersList from '../components/UsersList';
 
 const Users = () => {
   let users = [
-    // {
-    //   id:1,
-    //   name: 'enzo',
-    //   image: 'balbablal',
-    //   placeCount: 2
-    // }
+    {
+      id:1,
+      name: 'enzo',
+      image: 'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+      placeCount: 2
+    }
   ]
   return (
     <div>
